6ce472d69c8e44d488647741f35395a89add9bb9
Redstonemancz <vit.adamec@email.cz>
2018-11-23 19:53:20 +0100

    Update Hack2018.ino

--- Hack2018/Hack2018.ino
+++ Hack2018/Hack2018.ino
@@ -4,44 +4,51 @@ void setup()
   pinMode(27, OUTPUT); //červená
   pinMode(16, OUTPUT); //oranžová
   pinMode(17, OUTPUT); //zelená
-  pinMode(39, INPUT); //magnetický senzor
+  pinMode(22, INPUT); //magnetický senzor
   pinMode(18, OUTPUT); //červená 2
   pinMode(19, OUTPUT); //žlutá 2
   pinMode(23, OUTPUT); //zelená 2
   pinMode(25, OUTPUT); //zel
+  pinMode(37, INPUT); //magnetický senzor 2
 
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(analogRead(39));
+  digitalWrite(27, HIGH);
+  digitalWrite(18, HIGH);
+  Serial.print(analogRead(2));
+  Serial.println("   1");
+  Serial.print(analogRead(37));
+  Serial.println("   2");
+  if (analogRead(37)>4000)
+  {
+    zmena1();
+    zmenaBack1();
+  }
+  if (analogRead(2)>4000)
+  {
+    zmena2();
+    zmenaBack2();
+  }
   
 }
 
 void zmena1()
 {
   digitalWrite(27, HIGH);
-  delay(4000);
-  //  for (int i = 0; i < 5; i++) {
-  //    digitalWrite(16, HIGH);
-  //    digitalWrite(27,LOW);
-  //    delay(200);
-  //    digitalWrite(27, HIGH);
-  //    delay(200);
-  //    digitalWrite(27, LOW);
-  //  }
   digitalWrite(16, HIGH);
   delay(1500);
   digitalWrite(27, LOW);
   delay(500);
   digitalWrite(16, LOW);
   digitalWrite(17, HIGH);
+  delay(4000);
 
 }
 void zmenaBack1()
 {
   digitalWrite(17, HIGH);
-  delay(4000);
   for (int i = 0; i < 5; i++) {
     digitalWrite(17, LOW);
     delay(200);
@@ -55,27 +62,18 @@ void zmenaBack1()
 void zmena2()
 {
   digitalWrite(18, HIGH);
-  delay(4000);
-  //  for (int i = 0; i < 5; i++) {
-  //    digitalWrite(16, HIGH);
-  //    digitalWrite(27,LOW);
-  //    delay(200);
-  //    digitalWrite(27, HIGH);
-  //    delay(200);
-  //    digitalWrite(27, LOW);
-  //  }
   digitalWrite(19, HIGH);
   delay(1500);
   digitalWrite(18, LOW);
   delay(500);
   digitalWrite(19, LOW);
   digitalWrite(23, HIGH);
+  delay(4000);
 
 }
 void zmenaBack2()
 {
   digitalWrite(23, HIGH);
-  delay(4000);
   for (int i = 0; i < 5; i++) {
     digitalWrite(23, LOW);
     delay(200);
