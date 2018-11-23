02b85a522e6ac2e0fee473e33b4b667ed3d39788
Redstonemancz <vit.adamec@email.cz>
2018-11-23 16:21:38 +0100

    Create Hack2018.ino

---
+++ Hack2018/Hack2018.ino
@@ -0,0 +1,64 @@
+
+void setup()
+{
+  pinMode(27, OUTPUT); //červená
+  pinMode(16, OUTPUT); //oranžová
+  pinMode(17, OUTPUT); //zelená
+  pinMode(39, INPUT); //magnetický senzor fsg
+}
+
+void loop() {
+  digitalWrite(27, HIGH);
+  if (analogRead(39) == 0)
+  {
+    delay(20);
+    if (analogRead(39) == 0)
+    {
+      delay(20);
+      if (analogRead(39) == 0)
+      {
+        delay(20);
+        if (analogRead(39) == 0)
+        {
+          zmena();
+          zmenaBack();
+        }
+      }
+    }
+  }
+}
+
+void zmena()
+{
+  digitalWrite(27, HIGH);
+  delay(4000);
+  //  for (int i = 0; i < 5; i++) {
+  //    digitalWrite(16, HIGH);
+  //    digitalWrite(27,LOW);
+  //    delay(200);
+  //    digitalWrite(27, HIGH);
+  //    delay(200);
+  //    digitalWrite(27, LOW);
+  //  }
+  digitalWrite(16, HIGH);
+  delay(500);
+  digitalWrite(27, LOW);
+  delay(500);
+  digitalWrite(16, LOW);
+  digitalWrite(17, HIGH);
+
+}
+void zmenaBack()
+{
+  digitalWrite(17, HIGH);
+  delay(4000);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(17, LOW);
+    delay(200);
+    digitalWrite(17, HIGH);
+    delay(200);
+    digitalWrite(17, LOW);
+  }
+  digitalWrite(27, HIGH);
+
+}
